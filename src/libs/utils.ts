d980de607813a6c707f86a5d53e4d6a6f0149b1a
Taufik Oktama <ot@unforma.club>
2024-10-18 00:40:17 +0700

    feat: create `toSlug` method

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,7 +1,12 @@
 import { type ClassValue, clsx } from "clsx";
+import slugify from "slugify";
 
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function toSlug(s: string) {
+  return slugify(s, { lower: true, remove: /[*+~.()'"!:@]/g });
 }
