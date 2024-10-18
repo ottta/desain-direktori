0d026bcd388ee4d10fe0d6fdc4dd5eeef001cbdd
Taufik Oktama <ot@unforma.club>
2024-10-19 00:11:50 +0700

    fix: move header and footer to rootlayout

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,19 +1,5 @@
-import NextLink from "next/link";
 import { ReactNode } from "react";
 
-import { cn } from "@/libs/utils";
-
 export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <header
-        className={cn("fixed", "top-0", "right-0", "left-0", "bg-neutral-100")}
-      >
-        <div data-container>
-          <NextLink href="/">Index</NextLink>
-        </div>
-      </header>
-      <main className={cn("pt-12")}>{children}</main>
-    </>
-  );
+  return <>{children}</>;
 }
--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,17 +2,6 @@ import Providers from "./Providers";
 
 import { ReactNode } from "react";
 
-import { cn } from "@/libs/utils";
-
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-
 export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <Providers>
-      <Header />
-      <main className={cn("pt-16", "lg:pt-14")}>{children}</main>
-      <Footer />
-    </Providers>
-  );
+  return <Providers>{children}</Providers>;
 }
