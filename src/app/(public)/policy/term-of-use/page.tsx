45c05aef44fa28b0cfa66352aa1b598f96183e1c
Taufik Oktama <ot@unforma.club>
2024-10-23 23:14:44 +0700

    feat: new pages

---
+++ src/app/(public)/policy/term-of-use/page.tsx
@@ -0,0 +1,9 @@
+import { cn } from "@/libs/utils";
+
+export default function Page() {
+  return (
+    <div data-container>
+      <div className={cn("px-3")}>Term Of Use</div>
+    </div>
+  );
+}
